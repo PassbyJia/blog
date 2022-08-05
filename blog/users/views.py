91cdf059fd999151ac804979c9e7906f1106a594
PassbyJia <leibin2333@163.com>
2022-08-05 15:43:35 +0800

    '状态保持'

--- blog/users/views.py
+++ blog/users/views.py
@@ -66,13 +66,21 @@ class RegisterView(View):
         except DatabaseError as e:
             logger.error(e)
             return HttpResponseBadRequest('注册失败')
+
+        from django.contrib.auth import login
+        login(request,user)
         # 4.返回响应跳转到指定页面
         #暂时返回一个注册成功的信息，后期再实现跳转到指定页面
 
         # redirect 是进行重定向
         # reverse 是可以通过namespace:name 来获取到视图所对应的路由
-        return redirect(reverse('home:index'))
+        response = redirect(reverse('home:index'))
         # return HttpResponse('注册成功，重定向到首页')
+
+        #设置cookie信息，以方便首页中 用户信息展示的判断和用户信息的展示
+        response.set_cookie('is_login',True)
+        response.set_cookie('username',user.username,max_age=7*24*3600)
+        return response
 
 
 #图片验证码
