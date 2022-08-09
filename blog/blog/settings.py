79abe77756b9318e838840056a13296a0c4140cb
PassbyJia <leibin2333@163.com>
2022-08-09 18:54:44 +0800

    '文章分类后台管理'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -110,9 +110,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+#修改语言显示
+LANGUAGE_CODE = 'zh-Hans' #’en-us‘
+#修改时区
+TIME_ZONE = 'Asia/Shanghai'#'UTC'
 
 USE_I18N = True
 
